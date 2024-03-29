dc6b4e274af9cdf0a03254e46220afa258e53799
instinxt <akpandey1928@gmail.com>
2024-03-30 03:02:28 +0530

    Upgrade the UI
    - Updated the UI
    - Integrated nextUI
    - Date extraction code completed

--- src/components/ComponentBody.tsx
+++ src/components/ComponentBody.tsx
@@ -1,18 +1,43 @@
 import { Datepicker, Table } from "flowbite-react";
 import { useEffect, useState } from "react";
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
 import {
     db,
     collection,
     onSnapshot,
     query,
 } from './firebase';
-import "../App.css"
 
 export const ComponentBody = () => {
     const [generateReport, setGenerateReport] = useState(false);
     const [fetchData, setFetchData] = useState(false);
     const [downloadButton, enableDownloadButton] = useState(false);
     const [vehicleData, setVehicleData] = useState([])
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+    const [reportOption, setReportOption] = useState("");
+    const [Frequency, setFrequency] = useState("");
+
+
+    const handleStartDateChange = (date) => {
+        setStartDate(date);
+        console.log('Selected start date:', date);
+    };
+
+    const handleEndDateChange = (date) => {
+        setEndDate(date);
+        console.log('Selected end date:', date);
+    };
+
+    const handleReportSelect = (option) => {
+        setReportOption(option)
+        console.log('Selected Option for Report', option);
+    }
+
+    const handleFrequencySelect = (option) => {
+        setFrequency(option)
+        console.log('Selected Option for Frequency', option);
+    }
 
     const colRef = collection(db, 'VehicleData');
     const q = query(colRef);
@@ -31,15 +56,12 @@ export const ComponentBody = () => {
     function handleReportClick() {
         setGenerateReport(true);
         enableDownloadButton(true);
-        // Temporary Fetch simulation
-        setTimeout(() => {
-            setFetchData(true);
-        }, 1000);
+        setFetchData(true);
     }
 
     return (
-        <div id="componentBody" className="flex flex-col h-screen bg-gray-200 p-4 xl:ml-64">
-            <div id="componentHeader" className="flex flex-row bg-gray-300 p-4 mb-4 justify-between">
+        < div id="componentBody" className={generateReport ? "h-full flex flex-col bg-black p-4 xl:ml-60" : "h-screen flex flex-col bg-black p-4 xl:ml-60"} >
+            <div id="componentHeader" className="flex flex-row text-white p-4 mb-4 justify-between">
                 <div id="componentName" className=" p-2 mb-2 text-3xl">
                     Reports
                 </div>
@@ -47,69 +69,69 @@ export const ComponentBody = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                     </svg>
+
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
+
                     <span>Username</span>
+
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                     </svg>
                 </div>
             </div>
 
-            <div id="filter" className="flex flex-row bg-gray-300 p-4 mb-4 justify-between">
-                <div id="reportsFilter" className="flex flex-row  p-2 mb-2">
+            <div id="filter" className="flex flex-row text-white p-2 border-b-1 border-t-1 mb-4 justify-between items-center">
+                <div id="reportsFilter" className="flex flex-row p-2 mb-2">
 
-                    <button id="dropdownDefaultButton" data-dropdown-toggle="reportsDropdown" className="mr-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Reports
-                        <svg className="w-2.5 h-2.5 ms-3 ml-10" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-                        </svg>
-                    </button>
+                    {/* Reports Dropdown */}
+                    <Dropdown>
+                        <DropdownTrigger>
+                            <Button
+                                variant="bordered"
+                                className="text-white w-48 mr-5 items-center font-bold text-md"
+                            >
+                                Report
+                            </Button>
+                        </DropdownTrigger>
+                        <DropdownMenu
+                            onAction={handleReportSelect}
+                            aria-label="Action event example"
+                        >
+                            <DropdownItem key="Total Miles Driven">Total Miles Driven</DropdownItem>
+                            <DropdownItem key="Energy Consumption">Energy Consumption</DropdownItem>
+                            <DropdownItem key="Cost Analysis">Cost Analysis</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
 
-                    {/* <!-- Dropdown menu --> */}
-                    <div id="reportsDropdown" className="mr-4 z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                        <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                            <li>
-                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Total Miles Driven</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Energy consumption</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Cost analysis</a>
-                            </li>
-                        </ul>
-                    </div>
+                    {/* Frequency Dropdown */}
+                    <Dropdown>
+                        <DropdownTrigger>
+                            <Button
+                                variant="bordered"
+                                className="text-white w-48 mr-5 items-center font-bold text-md"
+                            >
+                                Frequency
+                            </Button>
+                        </DropdownTrigger>
+                        <DropdownMenu
+                            aria-label="Action event example"
+                            onAction={handleFrequencySelect}
+                        >
+                            <DropdownItem key="Daily">Daily</DropdownItem>
+                            <DropdownItem key="Weekly">Weekly</DropdownItem>
+                            <DropdownItem key="Monthly">Monthly</DropdownItem>
+                            <DropdownItem key="Yearly">Yearly</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
 
-                    <button id="dropdownDefaultButton" data-dropdown-toggle="frequencyDropdown" className="mr-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Frequency
-                        <svg className="w-2.5 h-2.5 ms-3 ml-10" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-                        </svg>
-                    </button>
 
-                    {/* <!-- Dropdown menu --> */}
-                    <div id="frequencyDropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                        <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                            <li>
-                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Daily</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Weekly</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Monthly</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Yearly</a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    {/* Date Picker */}
-                    <div className="flex flex-row items-center">
-                        <Datepicker className="mr-2" />
+                    {/* Date Range Picker */}
+                    <div className="flex flex-row items-center w-96">
+                        <Datepicker className="mr-2" onSelectedDateChanged={handleStartDateChange} />
                         <span>to</span>
-                        <Datepicker className="ml-2" />
+                        <Datepicker className="ml-2" onSelectedDateChanged={handleEndDateChange} />
                     </div>
 
                 </div>
@@ -118,7 +140,7 @@ export const ComponentBody = () => {
                         (
                             <div className="flex flex-row">
                                 <div id="downloadCSV" className="p-2 flex flex-row">
-                                    <button type="button" className="flex flex-row items-center text-white bg-black hover:bg-slate-700 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2">
+                                    <button type="button" className="flex flex-row border-2 items-center text-white bg-black hover:bg-slate-700 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2">
                                         Download CSV
                                     </button>
                                 </div>
@@ -143,31 +165,31 @@ export const ComponentBody = () => {
 
             </div>
 
-            <div className="flex flex-col bg-gray-300 p-4">
+            <div className="flex flex-col p-4 text-white">
                 <div className="p-2 mb-2">
                     <Table>
                         {generateReport &&
-                            <Table.Head>
-                                <Table.HeadCell>License Plate</Table.HeadCell>
-                                <Table.HeadCell>Make</Table.HeadCell>
-                                <Table.HeadCell>VIN</Table.HeadCell>
-                                <Table.HeadCell>Model</Table.HeadCell>
-                                <Table.HeadCell>Type</Table.HeadCell>
-                                <Table.HeadCell>Date</Table.HeadCell>
-                                <Table.HeadCell>Miles Driven</Table.HeadCell>
+                            <Table.Head className="text-white">
+                                <Table.HeadCell className="bg-[#1C1C26]">License Plate</Table.HeadCell>
+                                <Table.HeadCell className="bg-[#1C1C26]">Make</Table.HeadCell>
+                                <Table.HeadCell className="bg-[#1C1C26]">VIN</Table.HeadCell>
+                                <Table.HeadCell className="bg-[#1C1C26]">Model</Table.HeadCell>
+                                <Table.HeadCell className="bg-[#1C1C26]">Type</Table.HeadCell>
+                                <Table.HeadCell className="bg-[#1C1C26]">Date</Table.HeadCell>
+                                <Table.HeadCell className="bg-[#1C1C26]">Miles Driven</Table.HeadCell>
                             </Table.Head>
                         }
                         {fetchData &&
                             vehicleData.map(({ data: { licensePlate, make, VIN, model, type, date, milesDriven } }) => (
-                                <Table.Body className="text-black bg-slate-400">
+                                <Table.Body className="text-white bg-black">
                                     <Table.Row>
-                                        <Table.Cell>{licensePlate}</Table.Cell>
-                                        <Table.Cell>{make}</Table.Cell>
-                                        <Table.Cell>{VIN}</Table.Cell>
-                                        <Table.Cell>{model}</Table.Cell>
-                                        <Table.Cell>{type}</Table.Cell>
-                                        <Table.Cell>{date}</Table.Cell>
-                                        <Table.Cell>{milesDriven}</Table.Cell>
+                                        <Table.Cell className="border-b-1">{licensePlate}</Table.Cell>
+                                        <Table.Cell className="border-b-1">{make}</Table.Cell>
+                                        <Table.Cell className="border-b-1">{VIN}</Table.Cell>
+                                        <Table.Cell className="border-b-1">{model}</Table.Cell>
+                                        <Table.Cell className="border-b-1">{type}</Table.Cell>
+                                        <Table.Cell className="border-b-1">{date}</Table.Cell>
+                                        <Table.Cell className="border-b-1">{milesDriven}</Table.Cell>
                                     </Table.Row>
                                 </Table.Body>
                             ))
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,3 @@
-import overview from '../assets/overview.svg';
-import car from "../assets/Car.svg";
-import reportIcon from "../assets/Report.svg";
-import chargers from "../assets/Charger.svg";
-import driver from "../assets/Drivers.svg";
-import schedules from "../assets/Schedules.svg";
-import admin from "../assets/admin.svg";
 import Logo from "../assets/logo.png"
 
 export const Sidebar = () => {
@@ -18,51 +11,76 @@ export const Sidebar = () => {
                 </svg>
             </button>
 
-            <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full xl:translate-x-0" aria-label="Sidebar">
-                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-500 dark:bg-gray-800">
-                    <a href="https://electrifyitnow.com/" className="flex items-center ps-2.5 mb-5">
-                        <img src={Logo} className="w-[600px]" alt="Flowbite Logo" />
+            <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-60 h-screen transition-transform -translate-x-full xl:translate-x-0" aria-label="Sidebar">
+                <div className="h-full px-3 py-4 overflow-y-auto bg-[#1C1C26] dark:bg-gray-800">
+                    <a href="https://electrifyitnow.com/" className="flex items-center mb-5">
+                        <img src={Logo} className="w-20 text-white" alt="ElectrifyIt Logo" />
+                        <span className="text-white text-xl">Electrify</span>
+                        <span className="text-blue-700 text-xl">It</span>
                     </a>
-                    <ul className="space-y-2 font-medium">
+                    <ul className="space-y-2 mt-10 font-medium">
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <img width={'20px'} className="text-white" src={overview} alt="overview" />
+                            <a href="#" className="flex items-center p-2 text-white text-lg rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700 group">
+
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                                </svg>
                                 <span className="ms-3">Overview</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <img width={'20px'} src={car} alt="Vehicles" />{' '}
+                            <a href="#" className="flex items-center p-2 text-white text-lg rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700 group">
+
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+                                </svg>
                                 <span className="ms-3">Vehicles</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <img width={'20px'} src={chargers} alt="Chargers" />{' '}
+                            <a href="#" className="flex items-center p-2 text-white text-lg rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700 group">
+
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
+                                </svg>
                                 <span className="ms-3">Chargers</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <img width={'20px'} src={driver} alt="Drivers" />
+                            <a href="#" className="flex items-center p-2 text-white text-lg rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700 group">
+
+                                <svg className="w-6 h-6 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4H1m3 4H1m3 4H1m3 4H1m6.071.286a3.429 3.429 0 1 1 6.858 0M4 1h12a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1Zm9 6.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z" />
+                                </svg>
                                 <span className="ms-3">Drivers</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <img width={'20px'} src={schedules} alt="Schedules" />{' '}
+                            <a href="#" className="flex items-center p-2 text-white text-lg rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700 group">
+
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                    <path fill-rule="evenodd" d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z" clip-rule="evenodd" />
+                                </svg>
                                 <span className="ms-3">Schedules</span>
+
                             </a>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white bg-gray-100 dark:hover:bg-gray-700 group">
-                                <img width={'20px'} src={reportIcon} alt="Reports" />
+                            <a href="#" className="flex items-center p-2 text-white text-lg rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700 group">
+
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                    <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75ZM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 0 1-1.875-1.875V8.625ZM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 0 1 3 19.875v-6.75Z" />
+                                </svg>
+
                                 <span className="ms-3">Reports</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <img width={'20px'} src={admin} alt="Admin Panel" />{' '}
+                            <a href="#" className="flex items-center p-2 text-white text-lg rounded-lg dark:text-white hover:bg-gray-100 hover:text-black dark:hover:bg-gray-700 group">
+
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                    <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
+                                </svg>
                                 <span className="ms-3">Admin Panel</span>
                             </a>
                         </li>
